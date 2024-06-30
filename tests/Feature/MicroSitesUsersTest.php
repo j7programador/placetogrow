f7bc109113d1d8beeae6261b7c46b84ee7a8ea3e
j7programador <j7programador@gmail.com>
2024-06-30 15:44:41 -0500

    feature: Add mysql services

--- tests/Feature/MicroSitesUsersTest.php
+++ tests/Feature/MicroSitesUsersTest.php
@@ -10,6 +10,10 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
+/**
+ * @group testUser
+ */
+
 class MicroSitesUsersTest extends TestCase
 {
     public function testListUsers(): void
